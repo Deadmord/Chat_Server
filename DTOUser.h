8eb75dca56501979624cb39e6c88327f28259c7a
DanSizov <111688811+DanSizov@users.noreply.github.com>
2023-06-13 15:49:50 +0300

    small commit

--- DTOUser.h
+++ DTOUser.h
@@ -1,26 +1,26 @@
-//#ifndef DTOUSER_H
-//#define DTOUSER_H
-//
-//#include <QString>
-//#include <QObject>
-//#include "DBUser.h"
-//#include "User.h"
-//#include "Entities/entities.h"
-//
-//namespace DTOHelper {
-//
-//	class DTOUser : public QObject {
-//
-//		Q_OBJECT
-//
-//	public:
-//		DTOUser(QObject* parent = nullptr);
-//		static QSharedPointer<DBEntity::DBUser> convertToDBModel(const User& user_);
-//		static QSharedPointer<User> convertToEntity(const DBEntity::DBUser& db_user_);
-//	};
-//
-//}
-//
-//Q_DECLARE_METATYPE(DTOHelper::DTOUser)
-//
-//#endif
+#ifndef DTOUSER_H
+#define DTOUSER_H
+
+#include <QString>
+#include <QObject>
+#include "DBUser.h"
+#include "User.h"
+#include "Entities/entities.h"
+
+namespace DTOHelper {
+
+	class DTOUser : public QObject {
+
+		Q_OBJECT
+
+	public:
+		DTOUser(QObject* parent = nullptr);
+		static QSharedPointer<DBEntity::DBUser> convertToDBModel(const User& user_);
+		static QSharedPointer<User> convertToEntity(const DBEntity::DBUser& db_user_);
+	};
+
+}
+
+Q_DECLARE_METATYPE(DTOHelper::DTOUser)
+
+#endif
