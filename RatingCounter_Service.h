2b8141995d86c81ebf8651d429048fda9066b9d7
veregant <veregant@gmail.com>
2023-06-14 12:52:45 +0300

    fixed

--- RatingCounter_Service.h
+++ RatingCounter_Service.h
@@ -1,8 +1,37 @@
 #pragma once
-#include <qobject.h>
+#ifndef RATINGCOUNTER_SERVICE_H
+#define RATINGCOUNTER_SERVICE_H
+
+#include <QObject>
+#include <QMutex>
+#include <QString>
+#include <QMap>
+
+#include <plog/Log.h>
+
 class RatingCounter_Service :
     public QObject
 {
+    Q_OBJECT
 
+private:
+
+    static RatingCounter_Service* p_instance;
+    static QMutex mutex;
+    explicit RatingCounter_Service(QObject* parent = nullptr);
+    QMap<QString, qint32> rating_storage;
+
+public:
+
+    static RatingCounter_Service* getInstance();
+    RatingCounter_Service(const RatingCounter_Service&) = delete;
+    RatingCounter_Service& operator=(const RatingCounter_Service&) = delete;
+
+public slots:
+
+    void addRating(const QString user_login_, bool like_or_dis);
+    void updateRating();
 };
 
+
+#endif // !RATINGCOUNTER_SERVICE_H
