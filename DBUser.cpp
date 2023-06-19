45d9da461ea70a234a8666180aad06ffa0584498
Aniklogika4417 <anikalogika4417@gmail.com>
2023-06-19 08:48:20 +0300

    user update

--- DBUser.cpp
+++ DBUser.cpp
@@ -9,7 +9,7 @@ namespace DBEntity {
     void DBUser::setLogin(const QString& login_) { this->a_login = login_; }
     QString DBUser::getPassword() const { return this->a_password; }
     void DBUser::setPassword(const QString& password_) { this->a_password = password_; }
-    QByteArray DBUser::getUserpic() const { return this->a_userpic; }
+    QByteArray DBUser::getUserpic() const { return a_userpic; }
     void DBUser::setUserpic(const QByteArray& userpic_) { this->a_userpic = userpic_; }
     quint32 DBUser::getRating() const { return this->a_rating; }
     void DBUser::setRating(const quint32& rating_) { this->a_rating = rating_; }
